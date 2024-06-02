a61743940c0bbc109c1cf76fed43d90322b22321
Faiz Al <faizali786313@gmail.com>
2024-06-02 11:27:09 +0530

    data fetching related issues fixing

--- app/utils.jsx
+++ app/utils.jsx
@@ -1,10 +1,20 @@
 export const fetchData = async () => {
   try {
-    const res = await fetch(`/data.json`);
-    const data = await res.json();
-    return JSON.parse(JSON.stringify(data));
+    const baseUrl =
+      process.env.NODE_ENV === "production"
+        ? "https://batch-system-assignment-git-main-faiz-alis-projects.vercel.app"
+        : "http://localhost:3000";
+
+    const response = await fetch(`${baseUrl}/data.json`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
   } catch (error) {
-    // console.log(error);
-    throw new Error(error);
+    console.error("Error fetching data:", error);
+    throw error;
   }
 };
