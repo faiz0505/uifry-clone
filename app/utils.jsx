dabdde6bfe2275ab4406bbcea4d5b82d06c26781
Faiz Al <faizali786313@gmail.com>
2024-06-02 11:22:51 +0530

    change fetching data url

--- app/utils.jsx
+++ app/utils.jsx
@@ -1,6 +1,6 @@
 export const fetchData = async () => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data.json`);
+    const res = await fetch(`/data.json`);
     const data = await res.json();
     return JSON.parse(JSON.stringify(data));
   } catch (error) {
